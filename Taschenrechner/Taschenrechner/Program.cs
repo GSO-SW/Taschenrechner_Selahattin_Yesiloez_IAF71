df2c7caacc37aedfd6d8e9d5b427b80cf6083499
Selo-beep <54985075+Selo-beep@users.noreply.github.com>
2019-09-13 13:55:06 +0200

    Subraktionsverfahren hinzufügen

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -19,20 +19,24 @@ namespace Taschenrechner
             {
                 Console.Clear();
 
+                Console.Write("Welche Rechenoperation möchten Sie durchführen : + : * - \n Eingabe : ");
+                op = Convert.ToChar(Console.ReadLine());
+
                 Console.Write("Geben Sie die erste Zahl : ");
                 int zahl1 = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Geben Sie die zweite Zahl : ");
                 int zahl2 = Convert.ToInt32(Console.ReadLine());
-               
-                Console.Write("Welche Rechenoperation möchten Sie durchführen : + : * - \n Eingabe : ");
-                op = Convert.ToChar(Console.ReadLine());
-
+                
                 switch (op)
                 {
                     case '+':
                         Console.Write("\n\nDas Ergebnis : {0}", zahl1 + zahl2);
                         break;
+                    case '-':
+                        Console.Write("\n\nDas Ergebnis : {0}", zahl1 - zahl2);
+                        break;
+                    
                 }
 
                 
