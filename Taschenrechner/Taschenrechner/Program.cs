0c1ce4f2c6316d943f72254099cc1bf3d1c7fce3
Selo-beep <54985075+Selo-beep@users.noreply.github.com>
2019-09-13 13:47:56 +0200

    Addition mit Cases gemacht.

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -13,24 +13,37 @@ namespace Taschenrechner
 
             string choice = "";
 
+            char op = 'a';
+
             do
             {
                 Console.Clear();
-
 
                 Console.Write("Geben Sie die erste Zahl : ");
                 int zahl1 = Convert.ToInt32(Console.ReadLine());
 
                 Console.Write("Geben Sie die zweite Zahl : ");
                 int zahl2 = Convert.ToInt32(Console.ReadLine());
+               
+                Console.Write("Welche Rechenoperation möchten Sie durchführen : + : * - \n Eingabe : ");
+                op = Convert.ToChar(Console.ReadLine());
 
-                Console.Write("\n\nDas Ergebnis : {0}", zahl1 + zahl2);
+                switch (op)
+                {
+                    case '+':
+                        Console.Write("\n\nDas Ergebnis : {0}", zahl1 + zahl2);
+                        break;
+                }
+
+                
+
+                
                 Console.Write("\n\nErneut : J\\N");
                 Console.Write(" Entscheidung : ");
                 choice = Console.ReadLine();
             } while (choice == "J" || choice == "j");
         
 
-    }
+        }  
     }
 }
