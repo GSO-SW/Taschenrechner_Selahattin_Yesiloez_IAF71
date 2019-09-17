74d4b1c105a6401cf798b55c86688819970d2164
Selahattin Yesiloez <selahattiny@schule.koeln>
2019-09-17 23:17:06 +0200

    Potenzfunktion mit Eingabe des Exponenten

--- Taschenrechner/Taschenrechner/Program.cs
+++ Taschenrechner/Taschenrechner/Program.cs
@@ -14,20 +14,28 @@ namespace Taschenrechner
             string choice = "";
 
             char op = ' ';
-
+            double zahl1 = 0;
+            double zahl2 = 0;
+            int Exponent = 0;
             do
             {
                 Console.Clear();
 
                 Console.Write("Welche Rechenoperation möchten Sie durchführen : + / * - oder P für eine Potenz Rechnung\n Eingabe : ");
                 op = Convert.ToChar(Console.ReadLine());
-             
-                Console.Write("Geben Sie eine Zahl ein: ");
-                double zahl1 = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Geben Sie eine zweite Zahl : ");
-                double zahl2 = Convert.ToInt32(Console.ReadLine());
                 
+                Console.Write("Geben Sie eine Zahl ein: ");
+                zahl1 = Convert.ToDouble(Console.ReadLine());
+                if (op != 'p' && op != 'P') {
+                    Console.Write("Geben Sie eine zweite Zahl : ");
+                    zahl2 = Convert.ToDouble(Console.ReadLine());
+                }
+                else if (op == 'p' || op == 'P')
+                {
+                    Console.Write("Geben Sie bitte den Exponent ein : ");
+                    Exponent = Convert.ToInt32(Console.ReadLine());
+
+                }
                 switch (op)
                 {
                     case '+':
@@ -42,6 +50,23 @@ namespace Taschenrechner
                     case '/':
                         Console.Write("\n\nDas Ergebnis : {0}", zahl1 / zahl2);
                         break;
+                    case 'P':
+                        double a = zahl1;
+                        for (int i = 0; i < Exponent - 1; i++)
+                        {
+                            a = a * zahl1;
+                        }
+                        Console.Write("\n\nDas Ergebnis : {0}", a);
+                        break;
+                    case 'p':
+                        double b = zahl1;
+                        for (int i = 0; i < Exponent - 1; i++)
+                        {
+                            b = b * zahl1;
+                        }
+                        Console.Write("\n\nDas Ergebnis : {0}", b);
+                        break;
+
                 }
 
                 
